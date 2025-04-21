d61b3dc0bb639ebf1fc1939d33fff1774fc3ebe5
UtkarshSingh83 <121507514+UtkarshSingh83@users.noreply.github.com>
2025-04-21 23:53:40 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BSAE_PATH || "/API-Integration-State-Management"
 })
